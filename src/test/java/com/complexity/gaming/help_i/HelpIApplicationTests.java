274523c61d7120303df5c8d922bd1e64ad31856c
ColtMoons <coltmoonsac@gmail.com>
2021-05-06 16:06:36 -0500

    First commit

---
+++ src/test/java/com/complexity/gaming/help_i/HelpIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.complexity.gaming.help_i;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpIApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
