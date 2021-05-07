f073c2985bac7182cb0b9536e9220905e3fefe80
JoseRoncal2002 <u201912909@upc.edu.pe>
2021-05-06 19:29:22 -0500

    create-player-endpoint

--- src/main/java/com/complexity/gaming/help_i/domain/model/Person.java
+++ src/main/java/com/complexity/gaming/help_i/domain/model/Person.java
@@ -29,4 +29,39 @@ public class Person extends AuditModel {
     @NotNull
     private Date birthDate;
 
+    public long getId() {
+        return id;
+    }
+
+    public Person setId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Person setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Person setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public Person setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+        return this;
+    }
 }
---
+++ src/main/java/com/complexity/gaming/help_i/domain/model/Player.java
@@ -0,0 +1,6 @@
+package com.complexity.gaming.help_i.domain.model;
+
+public class Player extends Person {
+
+
+}
